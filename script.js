bc266a12bbf2b4fe8b553bf7d65ec3984794c9bf
julian <jdrc@protonmail.com>
2025-07-11 20:11:15 -0500

    add input state/misc fixes

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
+// TODO:
+//  - modal opening and closing
+
 const overlay = document.querySelector('.overlay')
 const burger = document.querySelector('.burger-menu')
+const text_inputs = document.querySelectorAll('.pledge-amount')
+const modal_pledge = document.querySelector('.modal-pledge')
+const rect = modal_pledge.getBoundingClientRect()
+
+text_inputs.forEach(input => 
+    input.addEventListener('click', () => 
+        input.querySelector('input[type="text"]').focus()
+    )
+)
+
+console.log(rect.width)
